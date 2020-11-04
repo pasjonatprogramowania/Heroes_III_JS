ecd6bdf5520646c66932e5967aae2e9652f0474c
PasjonatProgramowania <paw.szew1234@gmail.com>
2020-11-04 17:20:43 +0100

    Dzialajaca wersja kodu ale wszytskie testy w jedenej funkcji

--- js/creatures.js
+++ js/creatures.js
@@ -2,6 +2,9 @@ class Board {
     constructor() {
         this.map = new Map();
     }
+    returnMap() {
+        return this.map;
+    }
     add(point, creature) {
         if (this.map.has(point)) {
             throw 'Exception: => To pole jest zajete'
@@ -10,6 +13,12 @@ class Board {
     }
     getVal(point) {
         return this.map.get(point)
+    }
+    move(point, newPoint) {
+        let creature = this.map.get(point);
+        this.map.delete(point);
+        this.map.set(newPoint, creature)
+        console.log('this.map', this.map)
     }
 }
 
@@ -24,19 +33,34 @@ class Creature {
 
 }
 
+class BoardMovingTest {
+
+}
+
 function mapShouldHaveKeyAndValue() {
     let board = new Board();
     let creature = new Creature();
-    // let creature2 = new Creature();
 
-    let newPoint = new Point(0, 0)
-    board.add(newPoint, creature);
+    let unitTestPoint = new Point(0, 0)
+    board.add(unitTestPoint, creature);
+
+    // let unitTestPoint2 = new Point(1, 0)
+    // board.add(unitTestPoint2, creature);
+
+    // let creature2 = new Creature();
     // board.add(newPoint, creature2);
 
-    let cretureFromBoard = board.getVal(newPoint);
+    let newUnitTestPoint = new Point(1, 1)
+    board.move(unitTestPoint, newUnitTestPoint)
+
+
+    let cretureFromBoard = board.getVal((newUnitTestPoint || unitTestPoint));
+    console.log('cretureFromBoard', cretureFromBoard)
     return {
-        cretureFromBoard: (cretureFromBoard || 1), //Wysylanie undefined do test.js nie dziala wiec jesli jest undefined to zamiast undefined daj 1
+        //Wysylanie undefined do test.js nie dziala wiec jesli jest undefined to zamiast undefined daj 1
+        cretureFromBoard: (cretureFromBoard || 1),
         creature: creature,
+        map: board.returnMap()
     }
 }
 console.log('mapShouldHaveKeyAndValue', mapShouldHaveKeyAndValue())
