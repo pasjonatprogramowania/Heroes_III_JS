f36aae898c9ed7d706877b1d793ff417c2e58cc7
PasjonatProgramowania <paw.szew1234@gmail.com>
2020-11-04 21:22:07 +0100

    test commit to new branch

--- js/board.js
+++ js/board.js
@@ -6,27 +6,48 @@ import BoardTest from './test/boardTest.js'
 export default class Board {
     constructor() {
         this.map = new Map();
+        this.boardX = 20
+        this.boardY = 15
     }
     returnMap() {
         return this.map;
     }
     add(point, creature) {
+        let mapToString = ``;
+        this.map.forEach((val, key) => {
+            mapToString += ` ${JSON.stringify(key)} ${JSON.stringify(val)} `;
+        })
+
         if (this.map.has(point)) {
             throw 'Exception: => To pole jest zajete, nie mozesz tam dodac jednostki'
         }
+        if (point.x > this.boardX || point.y > this.boardY) {
+            throw 'Exception: => Creature nie zostala ruszona, wskazaany pkt jest poza mapa'
+        }
+
         this.map.set(point, creature)
+
+        if (mapToString.includes(JSON.stringify(point))) {
+            throw 'Exception: => Klucz nie jest równy tej wartosci która powinna byc wpisana'
+        }
+
     }
     getVal(point) {
         return this.map.get(point)
     }
     move(point, newPoint) {
         let mapToString = ``;
-        this.map.forEach((val, key, map) => {
+        this.map.forEach((val, key) => {
             mapToString += ` ${JSON.stringify(key)} ${JSON.stringify(val)} `;
         })
+
+        if (newPoint.x > this.boardX || newPoint.y > this.boardY) {
+            throw 'Exception: => Creature nie zostala ruszona, wskazaany pkt jest poza mapa'
+        }
         if (mapToString.includes(JSON.stringify(newPoint))) {
             throw 'Exception: => To pole jest zajete, nie mozesz tam ruszyc jednostki'
         }
+
         let creature = this.map.get(point);
         this.map.delete(point);
         this.map.set(newPoint, creature)
@@ -40,7 +61,6 @@ let boardTest = new BoardTest();
 
 boardTest.mapShouldHaveKeyAndValue()
 boardMovingTest.cretureShouldMoveCorrectly()
-boardMovingTest.creatureShoulCannotMoveToNotEmptyField()
 
 // module.exports = new Board();
 
--- js/test/boardMovingTest.js
+++ js/test/boardMovingTest.js
@@ -10,24 +10,6 @@ export default class BoardMovingTest {
         let unitTestPoint = new Point(0, 0)
         board.add(unitTestPoint, creature);
 
-        let newUnitTestPoint = new Point(1, 1)
-        board.move(unitTestPoint, newUnitTestPoint)
-
-        if (board.getVal(unitTestPoint)) {
-            throw 'Exception: => Creature nie zostala poprawnie usunieta, na jej starym miejscu jest slad po niej'
-        }
-        if (board.getVal(newUnitTestPoint) !== creature) {
-            throw 'Exception: => Creature nie zostala poprawnie usunieta, w jej nowym miejscu nie ma tej samej creatury'
-
-        }
-    }
-    creatureShoulCannotMoveToNotEmptyField() {
-        let board = new Board();
-        let creature = new Creature();
-
-        let unitTestPoint = new Point(0, 0)
-        board.add(unitTestPoint, creature);
-
         // let unitTestPoint2 = new Point(1, 1)
         // board.add(unitTestPoint2, creature);
 
